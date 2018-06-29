77685a3a8769b91d87ea9b2bb4c9fce34985689a
sanjul910bm <sanju.l910bm@gmail.com>
2018-06-29 08:06:03 +0545

    register activity adapter added

--- app/src/main/java/np/edu/nast/demoapp/demoapp/RegisterActivity.java
+++ app/src/main/java/np/edu/nast/demoapp/demoapp/RegisterActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -22,6 +23,7 @@ public class RegisterActivity extends AppCompatActivity {
     EditText etPassword;
     EditText etEmail;
     ImageButton btnShowHidePassword;
+    Button btnRegister;
     Context context;
     Spinner spinnerChooseSalary;
     RegisterModel registerModel;
@@ -39,6 +41,7 @@ public class RegisterActivity extends AppCompatActivity {
         etLastName = findViewById(R.id.et_lastname);
         etPassword = findViewById(R.id.et_sign_up_password);
         etEmail = findViewById(R.id.et_email);
+        btnRegister = findViewById(R.id.submit_view_registeration);
         ViewUtils.setupUI(findViewById(R.id.activity_register), this);
         btnShowHidePassword = findViewById(R.id.btn_show_hide_register_password);
         btnShowHidePassword.setImageResource(R.drawable.ic_eye);
@@ -47,24 +50,6 @@ public class RegisterActivity extends AppCompatActivity {
         context = getApplicationContext();
         registerModel = new RegisterModel();
 
-        String firstName = etFirstName.getText().toString().trim();
-        String lastName = etLastName.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
-        String email = etEmail.getText().toString().trim();
-
-
-        SharedPreferences pref = context.getSharedPreferences("MyPref", MODE_PRIVATE); // 0 - for private mode
-        SharedPreferences.Editor editor = pref.edit();
-
-        editor.putBoolean("boolValue", true); // Storing boolean - true/false
-        editor.putString("name", "Sanjay"); // Storing string
-        editor.putInt("rollno", 1); // Storing integer
-        editor.apply(); // commit changes
-
-        String name = pref.getString("name", ""); // getting String
-        int value = pref.getInt("rollno", 0); // getting Integer
-
-        Toast.makeText(this, "name" + name + "value" + value, Toast.LENGTH_SHORT).show();
 
         btnShowHidePassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,5 +69,42 @@ public class RegisterActivity extends AppCompatActivity {
         salaryAdapter.setDropDownViewResource(R.layout.item_spinner);
         spinnerChooseSalary.setAdapter(salaryAdapter);
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firstName = etFirstName.getText().toString().trim();
+                String lastName = etLastName.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+
+                registerModel.setFirstName(firstName);
+                registerModel.setLastName(lastName);
+                registerModel.setEmail(email);
+
+                if (spinnerChooseSalary.getSelectedItemPosition() > 0) {
+                    String selectedItem = spinnerChooseSalary.getSelectedItem().toString();
+                    registerModel.setSalary(selectedItem);
+                }
+            }
+        });
+
+
+        setAndGetSharedPrefs();
+
+
+    }
+
+    private void setAndGetSharedPrefs() {
+        SharedPreferences pref = context.getSharedPreferences("MyPref", MODE_PRIVATE); // 0 - for private mode
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putBoolean("boolValue", true); // Storing boolean - true/false
+        editor.putString("name", "Sanjay"); // Storing string
+        editor.putInt("rollno", 1); // Storing integer
+        editor.apply(); // commit changes
+
+        String name = pref.getString("name", ""); // getting String
+        int value = pref.getInt("rollno", 0); // getting Integer
+
+        Toast.makeText(this, "name" + name + "value" + value, Toast.LENGTH_SHORT).show();
     }
 }
